6fdb1d3f7631013642b91b314699ecfb5077c268
ZhouTian0716 <egglord0716@gmail.com>
2023-05-01 00:39:10 +1000

    image upload backend

--- app/Controllers/Http/AwsS3Controller.ts
+++ app/Controllers/Http/AwsS3Controller.ts
@@ -2,8 +2,9 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { generateUploadURL } from '../../../utils/aws/s3';
 
 export default class AwsS3Controller {
-  public async secureUrl({ response }: HttpContextContract) {
-    const url = await generateUploadURL();
+  public async secureUrl({ request, response }: HttpContextContract) {
+    const extension = request.qs().fileType;
+    const url = await generateUploadURL(extension);
     return response.status(200).json(url);
   }
 }
--- utils/aws/s3.ts
+++ utils/aws/s3.ts
@@ -1,5 +1,5 @@
 import Env from '@ioc:Adonis/Core/Env';
-import aws from "aws-sdk";
+import S3 from 'aws-sdk/clients/s3';
 import crypto from 'crypto';
 import { promisify } from 'util';
 const randomBytes = promisify(crypto.randomBytes);
@@ -7,24 +7,23 @@ const randomBytes = promisify(crypto.randomBytes);
 const region = Env.get('AWS_S3_REGION');
 const bucketName = Env.get('AWS_S3_BUCKET');
 const accessKeyId = Env.get('AWS_S3_ACCESS_KEY_ID');
-const secretAccessKey = Env.get('AWS_S3_ECRET_ACCESS_KEY');
+const secretAccessKey = Env.get('AWS_S3_SECRET_ACCESS_KEY');
 
-const s3 = new aws.S3({
+const s3 = new S3({
   region,
   accessKeyId,
   secretAccessKey,
   signatureVersion: 'v4',
 });
 
-export const generateUploadURL = async () => {
-  const rawBytes = await randomBytes(16);
+export const generateUploadURL = async (fileExtension:String) => {
+  const rawBytes = await randomBytes(6);
   const imageName = rawBytes.toString('hex');
-
-  // ZT-NOTE:The secureUrl only valid for 60 seconds
   const params = {
     Bucket: bucketName,
-    Key: imageName,
-    Expires: 60,
+    Key: `project-images/${imageName}.${fileExtension}`,
+    Expires: 5 * 60,
+    ContentType:`image/${fileExtension}`
   };
 
   const uploadURL = await s3.getSignedUrlPromise('putObject', params);

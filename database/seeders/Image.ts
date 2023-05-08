bd94a5bd0612d9e145e2153108869510be17001f
Ubuntu <ubuntu@ip-172-31-34-75.ap-southeast-2.compute.internal>
2023-05-08 13:48:30 +0000

    seeders updated to match s3

--- database/seeders/Image.ts
+++ database/seeders/Image.ts
@@ -8,32 +8,32 @@ export default class ImageSeeder extends BaseSeeder {
     const uniqueKey = 'url';
     await Image.updateOrCreateMany(uniqueKey, [
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000001.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000001.jpg',
         fileName: 'project-images/id-1/000000000001.jpg',
         projectId: 1,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000002.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000002.jpg',
         fileName: 'project-images/id-1/000000000002.jpg',
         projectId: 1,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000003.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000003.jpg',
         fileName: 'project-images/id-1/000000000003.jpg',
         projectId: 1,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000004.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-1/000000000004.jpg',
         fileName: 'project-images/id-1/000000000004.jpg',
         projectId: 1,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-2/000000000005.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-2/000000000005.jpg',
         fileName: 'project-images/id-2/000000000005.jpg',
         projectId: 2,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-2/000000000006.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-2/000000000006.jpg',
         fileName: 'project-images/id-2/000000000006.jpg',
         projectId: 2,
       },
@@ -43,17 +43,17 @@ export default class ImageSeeder extends BaseSeeder {
         projectId: 3,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-3/000000000008.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-3/000000000008.jpg',
         fileName: 'project-images/id-3/000000000008.jpg',
         projectId: 3,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-4/000000000009.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-4/000000000009.jpg',
         fileName: 'project-images/id-4/000000000009.jpg',
         projectId: 4,
       },
       {
-        url: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/project-images/id-4/000000000010.jpg',
+        url: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/project-images/id-4/000000000010.jpg',
         fileName: 'project-images/id-4/000000000010.jpg',
         projectId: 4,
       },
--- database/seeders/Profile.ts
+++ database/seeders/Profile.ts
@@ -9,12 +9,12 @@ export default class ProfileSeeder extends BaseSeeder {
     const uniqueKey = 'accountId';
     await Profile.updateOrCreateMany(uniqueKey, [
       {
-        avatar: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/account-images/id-1/12792782813c.jpg',
+        avatar: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/account-images/id-1/12792782813c.jpg',
         accountId: 1,
         bio: 'Hi, I am Zhou Tian. I am the profile for account one.',
       },
       {
-        avatar: 'https://meydit-media-storage.s3.ap-southeast-2.amazonaws.com/account-images/id-2/0128287182e9.jpg',
+        avatar: 'https://meydit-media-storage-production.s3.ap-southeast-2.amazonaws.com/account-images/id-2/0128287182e9.jpg',
         accountId: 2,
         bio: 'Hi, I am Zhou Tian. I am the profile for account two.',
       },
